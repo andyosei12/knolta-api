675bcc416871b958e4e2db7676bcd69c55525a3e
Andy Osei <andrew.owusu@novanest.net>
2022-11-24 11:16:45 +0000

    feat-sign in

--- src/handlers/auth.ts
+++ src/handlers/auth.ts
@@ -1,5 +1,5 @@
 import prisma from "../db";
-import { createJWT, hashPassword } from "../modules/auth";
+import { compareUserPassword, createJWT, hashPassword } from "../modules/auth";
 
 export const createUser = async (req, res) => {
   const user = await prisma.user.create({
@@ -9,6 +9,12 @@ export const createUser = async (req, res) => {
     },
   });
   const token = createJWT(user);
+  res.status(200);
+  res.json({ data: token });
+};
+
+export const signInUser = async (req, res) => {
+  const token = createJWT(req.user);
   res.status(200);
   res.json({ data: token });
 };
