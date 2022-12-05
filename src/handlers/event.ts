4a3feb6d2621940acb2d69c366c543fe56f55217
Andy Osei <andrew.owusu@novanest.net>
2022-12-05 13:47:14 +0000

    fix-add general error handling

--- src/handlers/event.ts
+++ src/handlers/event.ts
@@ -1,54 +1,74 @@
 import prisma from "../db";
 
-export const getEvents = async (req, res) => {
-  const events = await prisma.event.findMany();
-  res.status(200);
-  res.json({ data: events });
+export const getEvents = async (req, res, next) => {
+  try {
+    const events = await prisma.event.findMany();
+    res.status(200);
+    res.json({ data: events });
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const createEvent = async (req, res) => {
-  const event = await prisma.event.create({
-    data: {
-      date: req.body.date,
-      venue: req.body.venue,
-      name: req.body.name,
-    },
-  });
-  res.status(201);
-  res.json({ data: event });
+export const createEvent = async (req, res, next) => {
+  try {
+    const event = await prisma.event.create({
+      data: {
+        date: req.body.date,
+        venue: req.body.venue,
+        name: req.body.name,
+      },
+    });
+    res.status(201);
+    res.json({ data: event });
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const findEventById = async (req, res) => {
-  const event = await prisma.event.findUnique({
-    where: {
-      id: req.params.eventId,
-    },
-  });
-  res.status(200);
-  res.json({ data: event });
+export const findEventById = async (req, res, next) => {
+  try {
+    const event = await prisma.event.findUnique({
+      where: {
+        id: req.params.eventId,
+      },
+    });
+    res.status(200);
+    res.json({ data: event });
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const updateEvent = async (req, res) => {
-  const event = await prisma.event.update({
-    where: {
-      id: req.params.eventId,
-    },
-    data: {
-      date: req.body.date,
-      venue: req.body.venue,
-      name: req.body.name,
-    },
-  });
-  res.status(200);
-  res.json({ data: event });
+export const updateEvent = async (req, res, next) => {
+  try {
+    const event = await prisma.event.update({
+      where: {
+        id: req.params.eventId,
+      },
+      data: {
+        date: req.body.date,
+        venue: req.body.venue,
+        name: req.body.name,
+      },
+    });
+    res.status(200);
+    res.json({ data: event });
+  } catch (error) {
+    next(error);
+  }
 };
 
-export const deleteEvent = async (req, res) => {
-  const event = await prisma.event.delete({
-    where: {
-      id: req.params.eventId,
-    },
-  });
-  res.status(200);
-  res.json({ data: event });
+export const deleteEvent = async (req, res, next) => {
+  try {
+    const event = await prisma.event.delete({
+      where: {
+        id: req.params.eventId,
+      },
+    });
+    res.status(200);
+    res.json({ data: event });
+  } catch (error) {
+    next(error);
+  }
 };
