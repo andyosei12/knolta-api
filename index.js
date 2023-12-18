f5957173ba19a5f122454863c9ede6b388ca5f10
Andrew Osei-Owusu <nanaosei2089@gmail.com>
2023-12-18 15:29:59 +0000

    add database configuration

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
+import connect from './db/index.js';
 
 dotenv.config();
 const app = express();
@@ -8,6 +9,9 @@ const port = process.env.PORT || 5000;
 
 // using morgan middleware
 app.use(morgan('dev'));
+
+// connect to the database
+connect();
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello World!' });
